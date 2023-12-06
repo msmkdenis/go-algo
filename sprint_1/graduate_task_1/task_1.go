9ffb8955c42a84867bd227ec4a94154174a1c78f
Denis Burtsev <msmkdenis@gmail.com>
2023-12-06 20:04:56 +0300

    task_2

--- sprint_1/graduate_task_1/task_1.go
+++ sprint_1/graduate_task_1/task_1.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 )
 
+// https://contest.yandex.ru/contest/22450/run-report/102130766/
+
 /*
 Тимофей ищет место, чтобы построить себе дом.
 Улица, на которой он хочет жить, имеет длину n, то есть состоит из n одинаковых идущих подряд участков.
@@ -41,7 +43,13 @@ func main() {
 	length, _ := strconv.Atoi(s[0])
 	data, isZeroSlice := makeIntSlice(s[1], length)
 
-	zeroIndexes := make([]int, 0)
+	/*
+		Основная идея алгоритма:
+		1. Составить карту нулей (записывать индексы) в слайс zeroIndexes
+		2. Составлять расстояния с учетом карты нулей, избегая итерирования по всему слайсу с номерами домов.
+	*/
+
+	zeroIndexes := make([]int, 0) // составляем карту нулей (записываем индексы)
 	for i, v := range data {
 		if v == 0 {
 			zeroIndexes = append(zeroIndexes, i)
@@ -49,50 +57,50 @@ func main() {
 	}
 
 	distance := make([]int, length)
-	if isZeroSlice {
+	if isZeroSlice { // проверка на граничный случай - если в слайсе только нули
 		for i, _ := range data {
 			distance[i] = 0
 		}
 	} else {
-		zeroCounter := 0
+		zeroCounter := 0 // счетчик на первое вхождение, чтобы не проверять расстояния слева
 		previousZero := 0
 		for i, v := range data {
-			if v == 0 {
-				if zeroCounter == 0 {
+			if v == 0 { // двигаемся по слайсу с номерами домов, ищем вхождение нуля
+				if zeroCounter == 0 { // если это первое вхождение нуля среди номеров домов
 					zeroCounter++
 					k := 0
-					previousZero = i
-					for i >= 0 {
-						distance[k] = i
-						i--
-						k++
+					previousZero = i // запоминаем индекс нуля для последующих вычислений
+					for i >= 0 {     // двигаемся слева направо, заполняя расстояния
+						distance[k] = i // если индекс 0 например 3, то расстояние от края будет 3
+						i--             // в цикле уменьшаем расстояние от края до 0
+						k++             // увеличиваем индекс в слайсе расстояний
 					}
 					rightDistance := 1
-					for k < length {
+					for k < length { // заполняем расстояния справа от нуля до конца слайса расстояний
 						distance[k] = rightDistance
 						k++
 						rightDistance++
 					}
-				} else {
+				} else { // если это не первое вхождение нуля
 					zeroCounter++
-					k := previousZero
-					l := i - previousZero
+					k := previousZero     // ведём отсчет от предыдущего вхождения нуля, т.к. мы запоминали индекс ранее
+					l := i - previousZero // расстояние от предыдущего вхождения нуля до текущего
 					for l >= 0 {
-						if distance[k] > l {
+						if distance[k] > l { // в определенный момент расстояние до текущего нуля будет меньше расстояния до предыдущего
 							distance[k] = l
 						}
 						l--
 						k++
 					}
-					index := zeroCounter
-					if index+1 <= len(zeroIndexes)-1 {
+					index := zeroCounter               // индекс текущего вхождения нуля
+					if index+1 <= len(zeroIndexes)-1 { // проверяем, что у нас не граничное значение и справа еще есть нули
 						rightDistance := 1
-						for k < zeroIndexes[index+1] {
+						for k < zeroIndexes[index+1] { // заполняем расстояния справа от нуля до следующего вхождения нуля
 							distance[k] = rightDistance
 							k++
 							rightDistance++
 						}
-					} else {
+					} else { // если нет следующего вхождения нуля, то заполняем расстояния до конца слайса с номерами домов
 						rightDistance := 1
 						for k < length {
 							distance[k] = rightDistance
@@ -100,7 +108,7 @@ func main() {
 							rightDistance++
 						}
 					}
-					previousZero = i
+					previousZero = i // запоминаем индекс нуля для последующих вычислений
 				}
 			}
 		}
