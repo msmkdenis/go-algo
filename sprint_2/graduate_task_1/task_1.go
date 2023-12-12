3cecf371a8e3e427f19568bd25fe3640e9370376
Denis Burtsev <msmkdenis@gmail.com>
2023-12-12 18:11:53 +0300

    sprint_2_graduate

--- sprint_2/graduate_task_1/task_1.go
+++ sprint_2/graduate_task_1/task_1.go
@@ -2,67 +2,110 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type Deque struct {
-	data []int
-	head int
-	tail int
-	size int
+// https://contest.yandex.ru/contest/22781/run-report/102702736/
+
+/*
+-- ПРИНЦИП РАБОТЫ --
+Для реализации интерфейса Deque с константной сложностью вставки и удаления элементов в начало и конец воспользуемся
+слайсом с заранее заданным параметром capacity и имплементируем кольцевой буфер.
+Также Deque неограниченного размера с константной сложностью вставки и удаления в начало и конец можно реализовать например с помощью связных списков,
+но указанная реализация потребует больше памяти (необходимо также хранить указатели на следующие элементы).
+
+-- РЕАЛИЗАЦИЯ --
+Для реализации интерфейса Deque на основе слайса с заранее заданным параметром capacity требуется поддерживать информацию о:
+	- размере слайса
+	- количестве элементов в слайсе
+	- индексе головы очереди
+	- индексе хвоста очереди
+В процессе обработки команд динамически высчитываются индексы хвоста и головы очереди, операции вставки выполняются по индексу.
+
+-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+Отслеживая положения головы и хвоста очереди, а также осуществляя операции вставки и удаления в начало и конец по соответствующим индексам,
+мы добились того, что слайсу не требуются перестановки внутреннего массива при вставке элементов в начало.
+Таким образом все операции вставки и удаления в начало и конец будут выполняться за O(1).
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+Все операции интерфейса Deque выполняется с константной сложностью О(1)
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+Для хранения элементов потребуется O(n) памяти (в размере слайса).
+*/
+
+type SliceDeque struct {
+	capacity int
+	data     []int
+	head     int
+	tail     int
+	size     int
 }
 
-func NewDeque(capacity int) *Deque {
-	return &Deque{
-		data: make([]int, capacity),
+func NewSliceDeque(capacity int) *SliceDeque {
+	return &SliceDeque{
+		capacity: capacity,
+		data:     make([]int, capacity),
 	}
 }
 
-func (d *Deque) PushFront(x int) {
-	if d.size == len(d.data) {
-		fmt.Println("error")
-		return
+func (d *SliceDeque) PushFront(x int) error {
+	if d.isFull() {
+		return errors.New("error")
 	}
-	d.head = (d.head - 1 + len(d.data)) % len(d.data)
+	d.head = (d.head - 1 + d.capacity) % d.capacity
 	d.data[d.head] = x
 	d.size++
+	return nil
 }
 
-func (d *Deque) PushBack(x int) {
-	if d.size == len(d.data) {
-		fmt.Println("error")
-		return
+func (d *SliceDeque) PushBack(x int) error {
+	if d.isFull() {
+		return errors.New("error")
 	}
 	d.data[d.tail] = x
-	d.tail = (d.tail + 1) % len(d.data)
+	d.tail = (d.tail + 1) % d.capacity
 	d.size++
+	return nil
 }
 
-func (d *Deque) PopFront() (int, bool) {
-	if d.size == 0 {
-		fmt.Println("error")
-		return 0, false
+func (d *SliceDeque) PopFront() (int, error) {
+	if d.isEmpty() {
+		return 0, errors.New("error")
 	}
 	x := d.data[d.head]
-	fmt.Println(x)
-	d.head = (d.head + 1) % len(d.data)
+	d.head = (d.head + 1) % d.capacity
 	d.size--
-	return x, true
+	return x, nil
 }
 
-func (d *Deque) PopBack() (int, bool) {
-	if d.size == 0 {
-		fmt.Println("error")
-		return 0, false
+func (d *SliceDeque) PopBack() (int, error) {
+	if d.isEmpty() {
+		return 0, errors.New("error")
 	}
-	d.tail = (d.tail - 1 + len(d.data)) % len(d.data)
+	d.tail = (d.tail - 1 + d.capacity) % d.capacity
 	x := d.data[d.tail]
-	fmt.Println(x)
 	d.size--
-	return x, true
+	return x, nil
+}
+
+func (d *SliceDeque) isFull() bool {
+	return d.size == len(d.data)
+}
+
+func (d *SliceDeque) isEmpty() bool {
+	return d.size == 0
+}
+
+type Deque interface {
+	PushFront(x int) error
+	PushBack(x int) error
+	PopFront() (int, error)
+	PopBack() (int, error)
 }
 
 func main() {
@@ -83,26 +126,42 @@ func main() {
 	}
 
 	dequeueCapacity, _ := strconv.Atoi(inputData[1])
-	deque := NewDeque(dequeueCapacity)
-	processCommands(inputData[2:], deque)
+	sliceDeque := NewSliceDeque(dequeueCapacity)
+	processCommands(inputData[2:], sliceDeque)
 }
 
-func processCommands(inputData []string, deque *Deque) {
+func processCommands(inputData []string, deque Deque) {
 	for _, v := range inputData {
 		command := strings.Split(v, " ")
 		if command[0] == "push_back" {
 			x, _ := strconv.Atoi(command[1])
-			deque.PushBack(x)
+			err := deque.PushBack(x)
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 		if command[0] == "push_front" {
 			x, _ := strconv.Atoi(command[1])
-			deque.PushFront(x)
+			err := deque.PushFront(x)
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 		if command[0] == "pop_front" {
-			deque.PopFront()
+			x, err := deque.PopFront()
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(x)
+			}
 		}
 		if command[0] == "pop_back" {
-			deque.PopBack()
+			x, err := deque.PopBack()
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(x)
+			}
 		}
 	}
 }
